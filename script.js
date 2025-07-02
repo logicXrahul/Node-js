e7a34464aa0eafab9cb16ac672a47f10885e2425
logicXrahul <rahul.aiml.2002@gmail.com>
2025-07-02 12:18:53 +0530

    add some changes

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const info = require("./fruits");
+import { sum, PI } from "./func.js";
+
+console.log(sum(2,9));
+
+import { generate, count } from "random-words";
+
+console.log(generate(5));
+
+// const info = require("./fruits");
 // const SV = require("./math");
 
-
-console.log(info);
+  
+// console.log(info);
